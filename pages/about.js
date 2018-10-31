a4d08a0fce307b2daed2b28dad273e9f472fe144
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-30 21:14:05 -0700

    (feat) Link pages together with anchor tags

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ const About = () => {
   return (
     <div>
       <h1>About Page</h1>
+      <a href="./">Home</a>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ const Index = () => {
   return (
     <div>
       <h1>SSR with Next.js</h1>
+      <a href="./about">About</a>
     </div>
   );
 };
